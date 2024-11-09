9c4a5185866027347ab0fb38479dee6c284168e4
mateusjairan <mateusjairan@gmail.com>
2024-11-09 19:43:24 -0300

    Aprendendo tipos primitivos no PHP

---
+++ ex003/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Tipos primitivos no PHP</h1>
+    <?php
+    // $num = 300;
+    // echo "o valor da variável é $num";
+    $num = (int) "560";
+    var_dump($num);
+    
+    ?>
+</body>
+</html>
